0b58a4327dac1f50d7db30e7b9f1e91da6ecbd67
KosyakovSA <kosyakovsa@mail.ru>
2021-05-05 12:46:42 +0300

    openssl configs

--- rmlocks.sh
+++ rmlocks.sh
@@ -1,4 +1,6 @@
+sudo killall apt apt-get
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
