4f2218139546d3e624a235f2786bd3f248799b0b
KosyakovSA <kosyakovsa@mail.ru>
2021-05-23 18:16:42 +0300

    some mods

--- 06-installpg.sh
+++ 06-installpg.sh
@@ -1,20 +1,34 @@
-cd /work/cicd
+#cd /work/cicd
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo apt-get update
   sudo apt-get install postgresql-9.6 postgresql-contrib-9.6
 
 #set pgpassword and apply russian coding
-export PGPASSWORD="K33u2%t"
-psql -h localhost -U postgres -p 5432 -a -w -f ./pginit.sql
+#export PGPASSWORD="K33u2%t"
+
+OUTPUT="`hostname -I`"
+echo "${OUTPUT}"
+
+rm $PWD/basesettings.sql
+rm $PWD/defaultdb.backup
+
+#read -p "what address(site or ip) of sprint (settings)" SITEADDRESS
+touch $PWD/basesettings.sql
+echo "UPDATE settings.sys_settings set syssetvalue = trim('http://${OUTPUT}') WHERE syssetname like 'mainUrl%'" >> $PWD/basesettings.sql
+#echo "${OUTPUT}" >> $PWD/basesettings.sql
+#echo /b >> $PWD/basesettings.sql
+#echo >> $PWD/basesettings.sql
+
 wget --no-check-certificate http://demo.premierb.ru/sign/defaultdb.backup
+
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'K33u2%t';"
+
+sudo -u postgres psql -h localhost -U postgres -p 5432 -a -f $PWD/pginit.sql
 /usr/bin/pg_restore $PWD/defaultdb.backup -d bg -U postgres -h localhost -p 5432
 
 
-read -p "what address(site or ip) of sprint (settings)" SITEADDRESS
-touch ./basesettings.sql
-echo "UPDATE settings.sys_settings set syssetvalue = '$SITEADDRESS' WHERE syssetname like 'mainUrl%'" >> ./basesettings.sql
-psql -h localhost -U postgres -p 5432 -a -w -f $PWD/basesettings.sql
+psql -h localhost -U postgres -p 5432 -a -f $PWD/basesettings.sql
 
 
 
--- pginit.sql
+++ pginit.sql
@@ -1,11 +1,9 @@
-ALTER USER postgres PASSWORD 'K33u2%t';
+
 UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
 DROP DATABASE Template1;
 CREATE DATABASE template1 WITH owner=postgres ENCODING = 'UTF-8' lc_collate = 'en_US.utf8' lc_ctype = 'en_US.utf8' template template0;
 UPDATE pg_database SET datistemplate = TRUE WHERE datname= 'template1';
 
-CREATE DATABASE bg WITH OWNER = postgres ENCODING = 'UTF8' 
-    LC_COLLATE = 'Russian_Russia.1251'
-    LC_CTYPE = 'Russian_Russia.1251'
+CREATE DATABASE bg WITH OWNER = postgres ENCODING = 'UTF8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
